f8952965402e50d27185244c7e66a604f3c53658
Marek Rudova <marek.rudova@gmail.com>
2026-02-09 12:55:15 +0100

    watched global, block, function and nested scope

---
+++ sandbox/03-functions-scope/04-block-scope/script.js
@@ -0,0 +1,32 @@
+const x = 100;
+const foo = 1;
+var bar = 2;
+
+if (true) {
+  const y = 200;
+  console.log(x + y);
+}
+
+// console.log(x + y);
+
+for (var i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
+// console.log(i);
+
+if (true) {
+  const a = 500;
+  const b = 600;
+  var c = 700;
+}
+
+console.log(c);
+
+function run() {
+  var d = 100;
+  console.log(d);
+}
+
+run();
+// console.log(d);
